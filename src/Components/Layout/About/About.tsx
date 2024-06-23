e1b4b29f489053907510172281776c7b7caabad9
TsuriHefer <tsuri.he@gmail.com>
2024-06-24 00:18:33 +0300

    WIP - responsive for tablet and ipad

--- src/Components/Layout/About/About.tsx
+++ src/Components/Layout/About/About.tsx
@@ -1,6 +1,9 @@
 import "./About.css";
 import mobileGreyCircle from "../../../Images/mobile-grey-circle.png"
 import mobileOrangeCircle from "../../../Images/mobile-orange-circle.png"
+import profileImage from "../../../Images/character.png"
+
+
 export function About(): JSX.Element {
 
 
@@ -9,21 +12,30 @@ export function About(): JSX.Element {
         <div className="About">
 
             <div className="about_content">
-                <p className="about_intro">Hello My Name Is <span>Tsuri<span className="point">.</span></span></p>
+                
+                <div className="text-con">
 
-                <h1 className="about_title">a full stack web developer</h1>
+                    <p className="about_intro">Hello My Name Is <span>Tsuri<span className="point">.</span></span></p>
 
-                <p className="about_description">I’m passionate about Front-End, particularly React. having saying that I do backend too. I have a background in motion design and graphic design. in addition to expertise in UX/UI, client relations, and creativity.</p>
+                    <h1 className="about_title">a full stack web developer</h1>
 
-                <button className="about-btn">My Projects</button>
+                    <p className="about_description">I’m passionate about Front-End, particularly React. having saying that I do backend too. I have a background in motion design and graphic design. in addition to expertise in UX/UI, client relations, and creativity.</p>
+
+                    <button className="about-btn">My Projects</button>
+                </div>
+
+                <div className="profile-image-con">
+                    <img src={profileImage} alt="" />
+                </div>
 
             </div>
 
             <div className="img-cont">
                 <img src={mobileOrangeCircle} alt="" className="orange-circle" />
 
-                  <img src={mobileGreyCircle} alt="" className="grey-circle" />
+                <img src={mobileGreyCircle} alt="" className="grey-circle" />
             </div>
+
 
         </div>
 
